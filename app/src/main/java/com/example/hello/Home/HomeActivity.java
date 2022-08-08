06de7c64fabc475e152afcd337c289c36a070716
rlawjd10 <rlawjd10eun@gmail.com>
2022-08-08 14:40:18 +0900

    manifest file edit, home activity added

---
+++ app/src/main/java/com/example/hello/Home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.hello.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hello.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
